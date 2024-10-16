3976f49e0d18cfb2b7a562d2b2d835ac99c537ae
Shahbinshoaib <abdulrehmanarm3975@gmail.com>
2024-10-16 23:41:09 +0500

    Upload

--- Shape Change/Assets/Scripts/SectionTrigger.cs
+++ Shape Change/Assets/Scripts/SectionTrigger.cs
@@ -11,7 +11,7 @@ public class SectionTrigger : MonoBehaviour
         if (other.CompareTag("Player"))  // Ensure the player triggers the event
         {
             // Calculate the spawn position with a fixed X value (-174) and ahead of the player on the Z-axis
-            Vector3 spawnPosition = new Vector3(-174, 0, 0);
+            Vector3 spawnPosition = new Vector3(-173, 0, 0);
 
             // Instantiate the new road section at the calculated position
             Instantiate(roadSection, spawnPosition, Quaternion.identity);
